33670df30c6114ca09296bc792b960c01dee3450
Koen Westendorp <koensswestendorp@gmail.com>
2023-01-18 23:25:48 +0100

    Modify deploy-run.sh script rsync from -r to -a

--- deploy-run.sh
+++ deploy-run.sh
@@ -9,14 +9,8 @@ readonly PROJECT_NAME=zeevonk
 
 readonly TARGET_HOST=pi@raspberrypi
 readonly TARGET_PATH=/home/pi/rust/${PROJECT_NAME}
-#readonly TARGET_ARCH=aarch64-unknown-linux-gnu
-#readonly SOURCE_PATH=./target/${TARGET_ARCH}/release/${PROJECT_NAME}
 readonly SOURCE_PATH=.
 
-#cargo build --release --target=${TARGET_ARCH}
-#rsync ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
-#ssh -t ${TARGET_HOST} ${TARGET_PATH}
-
-rsync -r ${SOURCE_PATH}/src/       ${TARGET_HOST}:${TARGET_PATH}/src/
-rsync -r ${SOURCE_PATH}/Cargo.toml ${TARGET_HOST}:${TARGET_PATH}
+rsync -a ${SOURCE_PATH}/src/       ${TARGET_HOST}:${TARGET_PATH}/src/
+rsync -a ${SOURCE_PATH}/Cargo.toml ${TARGET_HOST}:${TARGET_PATH}/
 ssh -t ${TARGET_HOST} "cd ${TARGET_PATH} && cargo build --release && sudo ./target/release/zeevonk"
