2b0c4b313194d92a8f1fcab97cb6fcf9f8b85e99
BaukeWestendorp <baukewestendorp@gmail.com>
2023-05-07 16:53:12 +0200

    Fix deployment

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,11 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
-readonly TARGET_HOST=pi@raspberrypi
+readonly TARGET_HOST="$1"
 readonly TARGET_PATH=/home/pi/zeevonk
 readonly TARGET_ARCH=armv7-unknown-linux-gnueabihf
 readonly SOURCE_PATH=./target/${TARGET_ARCH}/release/zeevonk
 
-cargo build --release --target=${TARGET_ARCH}
+cross build --target=armv7-unknown-linux-gnueabihf --release --bin zeevonk
 rsync ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
 ssh -t ${TARGET_HOST} "sudo ${TARGET_PATH}"
