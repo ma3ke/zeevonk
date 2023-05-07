ce7bc908bc83086e3f49eea09b66f40fd0755912
BaukeWestendorp <baukewestendorp@gmail.com>
2023-05-07 17:28:29 +0200

    Update simvonk welcome message

--- simvonk/src/main.rs
+++ simvonk/src/main.rs
@@ -8,11 +8,11 @@ mod renderer;
 const ADDRESS: &str = "0.0.0.0:7200";
 const FRAMES_PER_SECOND: f64 = 50.0;
 const WELCOME_MESSAGE: &str = r#"
-                                                       888
-    8P d8P  ,e e,   ,e e,  Y8b Y888P  e88 88e  888 8e  888 ee
-    P d8P  d88 88b d88 88b  Y8b Y8P  d888 888b 888 88b 888 P
-     d8P d 888   , 888   ,   Y8b "   Y888 888P 888 888 888 b
-    d8P d8  "KoeN"  "BauX"    Y8P     "88 88"  888 888 888 8b
+          ,e,                                         888    
+     dP"Y  "  888 888 8e  Y8b Y888P  e88 88e  888 8e  888 ee 
+    C88b  888 888 888 88b  Y8b Y8P  d888 888b 888 88b 888 P  
+     Y88D 888 888 888 888   Y8b "   Y888 888P 888 888 888 b  
+    d,dP  888 888 888 888    Y8P     "88 88"  888 888 888 8b 
     
                 By Koen & Bauke Westendorp, 2023.
 "#;
